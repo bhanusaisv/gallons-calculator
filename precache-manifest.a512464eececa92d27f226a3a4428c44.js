5c9f0bd4fecb75854c3d473d905815e2732ef542
bhanusaisv <bhanusaikumar.sv@gmail.com>
2020-08-29 21:47:30 +0530

    Updates

---
+++ precache-manifest.a512464eececa92d27f226a3a4428c44.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc343a17394bc59c822b2496d3bd7462",
+    "url": "/gallons-calculator/index.html"
+  },
+  {
+    "revision": "e2335a2e2d6bb6d6c9e9",
+    "url": "/gallons-calculator/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "688ede327816322d94b2",
+    "url": "/gallons-calculator/static/css/main.992a28c0.chunk.css"
+  },
+  {
+    "revision": "e2335a2e2d6bb6d6c9e9",
+    "url": "/gallons-calculator/static/js/2.16ae84f5.chunk.js"
+  },
+  {
+    "revision": "e0546182649f50a2eb923c2a881ed896",
+    "url": "/gallons-calculator/static/js/2.16ae84f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "688ede327816322d94b2",
+    "url": "/gallons-calculator/static/js/main.36d9dcdf.chunk.js"
+  },
+  {
+    "revision": "b3b7fcb91953827f4006",
+    "url": "/gallons-calculator/static/js/runtime-main.996b6476.js"
+  },
+  {
+    "revision": "1ee98de0f55dfc42e1182572180b1fb4",
+    "url": "/gallons-calculator/static/media/almond-wallpaper.1ee98de0.jpg"
+  }
+]);
